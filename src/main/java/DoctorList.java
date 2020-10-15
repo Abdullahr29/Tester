922176a6746101b290ac8b3d7e596ee9823ef3f0
Abdullah Rehman <ayr18@ic.ac.uk>
2020-10-15 10:42:27 +0100

    Initial commit

---
+++ src/main/java/DoctorList.java
@@ -0,0 +1,17 @@
+public class DoctorList {
+    private String[] docList;
+    private int count = 0;
+    public DoctorList(){
+        docList = new String[10];
+        docList[0] = "Dr Mireia";
+        count = 1;
+    }
+
+    public void addDoctor(String name){
+        docList[count] = name;
+    }
+
+    public String getName(int i){
+        return docList[i];
+    }
+}
